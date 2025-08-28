38c1ccc337b45bd9e4269c6be7c531efbdec69e7
fanioz <1700770+fanioz@users.noreply.github.com>
2025-08-28 09:42:52 +0700

    feat(deploy): integrate Astro/Vercel adapter
    Installs the `@astrojs/vercel` package and configures the project for server-side rendering on Vercel.
    
    - Adds the Vercel serverless adapter to `astro.config.mjs`.
    - Sets the output mode to 'server' to enable SSR capabilities.
    - Updates dependencies in `package.json` and `pnpm-lock.yaml`.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,13 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 import react from '@astrojs/react';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://teknopulse.vercel.app',
-  integrations: [tailwind(), sitemap(), react()],
+  output: 'server',
+  integrations: [tailwind(), sitemap(), react(), vercel()],
   build: {
     inlineStylesheets: 'auto',
   },
